2bb85377086b4869ef279645895975eba68bb691
FKGC <132376364+FKGC@users.noreply.github.com>
2024-04-16 23:42:41 +0800

    Update main.py

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ import time
 from collections import defaultdict
 from dataloader import train_generate
 import random
-from CIAN import CIAN  # ************************
+from SuperRL import MRL  # ************************
 import numpy as np
 import json
 torch.set_num_threads(1)
